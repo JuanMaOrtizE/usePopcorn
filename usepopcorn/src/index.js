4f8fbfe3ebc5b78ea277403307ee009835eb82e4
OrtizJM <53711761+JuanMaOrtizE@users.noreply.github.com>
2025-02-07 22:36:38 -0500

    2

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./starRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./starRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    ></StarRating>
+    ></StarRating> */}
   </React.StrictMode>
 );
